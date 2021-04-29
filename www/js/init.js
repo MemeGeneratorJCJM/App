33276965937700ed48fd3ee2aa146d894b6a159f
JCapelDev <win200003@gmail.com>
2021-04-29 18:29:51 +0200

    register requeriments button updated

--- www/js/init.js
+++ www/js/init.js
@@ -1,7 +1,7 @@
 (function($){
   $(function(){
 
-    $('.sidenav').sidenav();
+    // $('.sidenav').sidenav();
    
     
 
@@ -10,3 +10,37 @@
 $("#btnRegister").click(function(){
   window.open("register.html","blank");
 });
+
+function validateLogin() {
+  var pass = $('#pwd').val();
+  
+  var query = {
+    "username": $('#email').val(),
+    "password": pass
+
+  }
+  $.ajax({
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    url: "https://memegenerator-jcg-jmm.herokuapp.com/login/user",
+    contentType: "application/json",
+    crossDomain: true,
+    dataType: "json",
+    data: JSON.stringify(query),
+  }).done(function (response) {
+   
+
+
+  }).fail(function (response) {
+    if (response.responseJSON != undefined) {
+      alert(response.responseJSON.statusData);
+    } else {
+      alert("Error de conexión");
+    }
+    
+  });
+}
+
+$("#btnLogin").click(validateLogin);
--- www/js/register.js
+++ www/js/register.js
@@ -1,16 +1,61 @@
 (function($){
     $(function(){
   
-      $('.sidenav').sidenav();
+    //   $('.sidenav').sidenav();
      
       
   
     }); // end of document ready
   })(jQuery); 
+  $("#username").click(updateRegisterbtn);
+  $("#email").click(updateRegisterbtn);
+  $("#pwd").click(updateRegisterbtn);
+  $("#cpwd").click(updateRegisterbtn);
+  function updateRegisterbtn(){
+    if($("#username").val()!="" && $("#email").val()!="" && $("#pwd").val()!="" && $("#cpwd").val()!=""){
+      if($("#pwd").val()==$("#cpwd").val()){
+        $("#btnRegister").attr('class','waves-effect waves-light btn centrar');
+      }else{
+        $('#btnRegister').attr('class','waves-effect waves-light btn centrar disabled');
+      }
+    }
+  }
   $("#btnRegister").click(function(){
       //Launch AJAX query for register
-
-
+      //registerUser();
       //Open login
     window.open("index.html","blank");
   });
+
+  function registerUser() {
+    var pass = $('#pwd').val();
+    
+    var query = {
+      "username": $('#username').val(),
+      "email": $('#email').val(),
+      "password": pass
+  
+    }
+    $.ajax({
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: "https://memegenerator-jcg-jmm.herokuapp.com/login/user",
+      contentType: "application/json",
+      crossDomain: true,
+      dataType: "json",
+      data: JSON.stringify(query),
+    }).done(function (response) {
+     
+  
+  
+    }).fail(function (response) {
+      if (response.responseJSON != undefined) {
+        alert(response.responseJSON.statusData);
+      } else {
+        alert("Error de conexión");
+      }
+      
+    });
+  }
